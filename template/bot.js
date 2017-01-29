786e220a98cc1643c80dc3329a80ad7204949bff
Nick Pfisterer <nick@lunamachine.net>
2017-01-28 19:24:00 -0800

    Add command handler to templates

--- template/bot.js
+++ template/bot.js
@@ -2,21 +2,26 @@
 
 // Import dependencies
 const Discord = require('discord.js')
-const bot = require('commander')
+const cli = require('commander')
 const log = require('lloogg')
+const glob = require('glob')
+const fs = require('fs')
 
 // Check arguments for token and owner ID
-bot
+cli
   .version(require('./package.json').version)
   .option('-t, --token <token>', 'Add bot token')
   .option('-o, --owner <id>', 'Add owner ID')
   .parse(process.argv)
 
 // Do not proceed without a token
-if (!bot.token) {
+if (!cli.token) {
   log.error('Bot token required. Run:', 'node bot.js -t bot_token_here')
   process.exit(1)
 }
+
+// Load bot config
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 
 // Create a Discord client instance
 const client = new Discord.Client()
@@ -28,12 +33,30 @@ client.on('message', message => {
 
   // Do not proceed without command prefix
   if (!message.content.startsWith(config.prefix)) return
+
+  // Parse command
+  let command = message.content.split(' ')[0].slice(config.prefix.length)
+
+  // Parse arguments
+  let args = message.content.split(' ').slice(1)
+
+  // Find command
+  glob(`commands/**/${command}.js`, (err, files) => {
+    // Handle errors
+    if (err) return log.error(err)
+
+    // Load command
+    let command = require(`./${files[0]}`)
+
+    // Run command
+    command.run(client, message, args)
+  })
 })
 
 // Log when the bot is ready to receive information from Discord
 client.on('ready', () => {
-  log.success('I am ready!')
+  log.success('Bot is ready!')
 })
 
 // Log in to Discord
-client.login(bot.token)
+client.login(cli.token)
